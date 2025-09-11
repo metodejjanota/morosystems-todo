57d158e1e091d7a6cd558b0019cc9f60f7dad4dd
Metoděj Janota <metodejjanota@gmail.com>
2025-09-11 13:14:17 +0200

    global css update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
 	return (
-		<div className="bg-red-600">
+		<div>
 			<p>swag</p>
+			<Button>Click me</Button>
 		</div>
 	);
 }
