c27be9bdad4c78587cb8923637036d77a0525b05
Metoděj Janota <metodejjanota@gmail.com>
2025-09-11 14:37:47 +0200

    Loading state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store/store";
 import { fetchTodos } from "./store/todosSlice";
-import { Loader2Icon } from "lucide-react";
 import TodoComp from "./components/ui/todo";
+import { Skeleton } from "./components/ui/skeleton";
 
 function App() {
 	const dispatch = useDispatch<AppDispatch>();
@@ -22,7 +22,13 @@ function App() {
 			<h1 className="text-5xl font-bold text-center">Morosystems To-Do</h1>
 
 			<div className="flex justify-center items-center mt-10">
-				{loading && <Loader2Icon className="animate-spin h-10 w-10" />}
+				{loading && (
+					<div className="space-y-2 w-full lg:w-1/2 mx-2">
+						<Skeleton className="h-16 w-full" />
+						<Skeleton className="h-16 w-full" />
+						<Skeleton className="h-16 w-full" />
+					</div>
+				)}
 				{error && <p className="text-red-500">Chyba: {error}</p>}
 				{!loading && !error && todos.length === 0 && (
 					<p>Žádné úkoly k zobrazení</p>
